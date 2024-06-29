ac5224de1187d7e83ec3b12d3d44126fe9c06246
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-29 23:20:02 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,20 @@ function App() {
           <div> 
             <label htmlFor="">First Name *</label> <br />
             <input type="text" name="first-name" id="first_name" />
+            <span>This field is required</span>
           </div>
           <div>
             <label htmlFor="">Last Name *</label> <br />
             <input type="text" name="last-name" id="last_name" />
+            <span>This field is required</span>
+
           </div>
         </div>
         <div className='email'>
           <label htmlFor="">Email Address *</label> <br />
           <input type="text" name="email" id="email" />
+          <span>Please enter a valid email address</span>
+
         </div>
       
         <div class="query-type">
@@ -32,20 +37,28 @@ function App() {
           <div class="radio-option">
             <input type="radio" id="general-enquiry" name="query-type" value="general-enquiry"/>
             <label for="general-enquiry"> General Enquiry</label>
+
           </div>
           <div class="radio-option">
             <input type="radio" id="support" name="query-type" value="support"/>
             <label for="support"> Support</label>
+
           </div>
+
         </div>
+        <span>Please select a query type</span>
+
         <p class="form-alert" style={{display: 'none'}}>Please select a query type</p>
       </div>
       <div className='text-area'>
         <label htmlFor="">Message *</label>
         <textarea name="" id="" cols="5" rows="5"></textarea>
+        <span>This field is required</span>
+
       </div>
       <div className='check-box'>
-        <input type="checkbox" name="checkbox" id="check-box" /> <span style={{marginLeft: '1em'}}>I consent to be bein contacted by the team</span>
+        <input type="checkbox" name="checkbox" id="check-box" /> <span style={{marginLeft: '1em'}}>I consent to be bein contacted by the team</span> <br />
+        <span>To submit this form please consent to being contacted</span>
       </div>
       <button>Submit</button>
         </div>
