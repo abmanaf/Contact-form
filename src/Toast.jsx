b9eb3970c017197677c5476fc4e97d0f657b6553
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-01 18:36:39 +0000

    minor changes

--- src/Toast.jsx
+++ src/Toast.jsx
@@ -6,9 +6,9 @@ const Toast = () => {
     <div className="toast-container">
       <h2 className="toast-title">
         <img src="/assets/images/icon-success-check.svg" />
-        <span>Message Sent!</span>
+        <span style={{color: 'white'}}>Message Sent!</span>
       </h2>
-      <p className="toast-desc">
+      <p style={{fontSize: '10px'}} className="toast-desc">
         Thanks for completing the form. We'll be in touch soon!
       </p>
     </div>
